36d81295ce101a350dfcdf50d3dd48a3bc6f45d3
klau <klaudia.devel@gmail.com>
2015-12-12 16:08:43 +0100

    Afegir blog templates

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,13 +4,11 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('blog.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
+#if settings.DEBUG:
+#   urlpatterns += static(settings.STATIC_URL,
+#                         document_root=settings.STATIC_ROOT)
